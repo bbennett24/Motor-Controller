3b2afcc834a6a67c4a972aa15a28810f0b646610
bbennett24 <88847430+bbennett24@users.noreply.github.com>
2022-03-14 11:30:20 -0600

    Update main.py

--- main.py
+++ main.py
@@ -16,22 +16,22 @@ GPIO.setup(Forward, GPIO.OUT)
 GPIO.setup(Backward, GPIO.OUT)
 
 def forward(x):
-GPIO.output(Forward, GPIO.HIGH)
-print("Moving Forward")
-time.sleep(x)
-GPIO.output(Forward, GPIO.LOW)
+        GPIO.output(Forward, GPIO.HIGH)
+        print("Moving Forward")
+        time.sleep(x)
+        GPIO.output(Forward, GPIO.LOW)
 
 def reverse(x):
-GPIO.output(Backward, GPIO.HIGH)
-print("Moving Backward")
-time.sleep(x)
-GPIO.output(Backward, GPIO.LOW)
+        GPIO.output(Backward, GPIO.HIGH)
+        print("Moving Backward")
+        time.sleep(x)
+        GPIO.output(Backward, GPIO.LOW)
 
 while (1):
-
-forward(5)
-
-reverse(5)
-GPIO.cleanup()
+        
+        forward(5)
+        
+        reverse(5)
+        GPIO.cleanup()
 
     
